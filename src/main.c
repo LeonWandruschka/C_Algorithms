51f94b653317bbc75e80026a26e7eca168fbff97
Leon Wandruschka <leon.wandruschka@outlook.com>
2022-06-10 23:37:51 +0200

    Documentation

--- src/main.c
+++ src/main.c
@@ -3,7 +3,11 @@
 #include "recursiveFib.h"
 #include "dynamicFib.h"
 
-
+/**
+ * @brief This is the main function remove the // befor the function you want to test.
+ * 
+ * @return int 
+ */
 int main(void) // int argc, char const *argv[]
 {
     // RecursiveFib(10);   //Works fine for smaller numbers -> Really slow with higher numbers >= 40

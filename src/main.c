588c9cd0cb4a69b39cf03e5fe31b0432bb5a1af5
Leon Wandruschka <leon.wandruschka@outlook.com>
2022-06-10 22:53:01 +0200

    current main.c file

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "recursiveFib.h"
+#include "dynamicFib.h"
+
+
+int main(void) // int argc, char const *argv[]
+{
+    // RecursiveFib(10);   //Works fine for smaller numbers -> Really slow with higher numbers >= 40
+    // DynamicFib(60);     //Really fast also works with higher numbers
+
+    return 0;
+}

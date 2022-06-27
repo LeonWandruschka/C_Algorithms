08d5219e4946e9e83382aba3741cdba306245be9
Leon Wandruschka <leon.wandruschka@outlook.com>
2022-06-27 21:55:00 +0200

    Simplistic function calls

--- src/main.c
+++ src/main.c
@@ -1,24 +1,84 @@
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
 
 #include "recursiveFib.h"
 #include "dynamicFib.h"
 #include "patternSearch.h"
 #include "stack.h"
-#include "queue.h"
+//#include "queue.h"
+
+
+void FunctionCall();
+
 
 /**
  * @brief This is the main function remove the // befor the function you want to test.
  */
 int main(void) // int argc, char const *argv[]
 {
-    // Stack();            //Creating your own Stack to Push and pop your data
-    // Queue();            //Creating your own Queue to Push and pop your data
-    // RecursiveFib(10);   //Works fine for smaller numbers -> Really slow with higher numbers >= 40
-    // DynamicFib(60);     //Really fast also works with higher numbers
-    // PatternSearch();    //Search for a specific pattern in a Text
-
-    
+    FunctionCall();  
 
     return 0;
 }
+
+
+/**
+ * @brief Easy access to all functions of the repository
+ * 
+ */
+void FunctionCall()
+{
+    while(1)
+    {
+        char func;
+        printf("Which function du you want to test? ");
+        scanf("%c", &func);
+
+        switch (func)
+        {
+        case 'a':
+            //Works fine for smaller numbers -> Really slow with higher numbers >= 40
+            RecursiveFib(10);
+            exit(0);
+            break;
+        case 'b':
+            //Really fast also works with higher numbers
+            DynamicFib(60);
+            exit(0);
+            break;
+        case 'c':
+            //Search for a specific pattern in a Text
+            PatternSearch();
+            exit(0);
+            break;
+        case 'd':
+            //Creating your own Stack to Push and pop your data
+            Stack();
+            exit(0);
+            break;
+        case 'e':
+            //Creating your own Queue to Push and pop your data
+            // Queue();
+            exit(0);
+            break;
+
+        case 'X':
+            //Exitiong the programm
+            exit(0);
+
+        default:
+            printf("Enter the following characters to call the specific functions:\n");
+            printf("(a): Recursive Fibonacci\n");
+            printf("(b): Dynamic Fibonacci\n");
+            printf("(c): Patternsearch\n");
+            printf("(d): Stack\n");
+            printf("(e): Queue\n");
+            printf("(X): Exit the programm\n");
+            break;
+        }
+    }
+}
+
+
+
+
